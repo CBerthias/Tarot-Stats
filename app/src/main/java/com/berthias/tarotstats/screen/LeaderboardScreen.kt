fa0971e7dd3b06680be4fd8cfbb918a562e30d53
CBERTHIAS <cberthias-ext@mgen.fr>
2024-10-25 16:50:27 +0200

    Classement des appelés

--- app/src/main/java/com/berthias/tarotstats/screen/LeaderboardScreen.kt
+++ app/src/main/java/com/berthias/tarotstats/screen/LeaderboardScreen.kt
@@ -4,12 +4,18 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.DrawerState
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.PrimaryTabRow
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Tab
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.SpanStyle
@@ -53,44 +59,61 @@ fun LeaderboardScreen(
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LeaderboardScreenContent(modifier: Modifier = Modifier, listePartiesUI: List<PartieUI>) {
     val nbPartiesJoueur: MutableMap<String, Int> = HashMap()
     val nbWinJoueur: MutableMap<String, Int> = HashMap()
+    val nbPartieAppele: MutableMap<String, Int> = HashMap()
+    val nbWinAppele: MutableMap<String, Int> = HashMap()
     listePartiesUI.forEach { partieUI ->
         nbPartiesJoueur[partieUI.nomJoueur] =
             nbPartiesJoueur.getOrDefault(partieUI.nomJoueur, 0) + 1
+        nbPartieAppele[partieUI.coequipier] =
+            nbPartieAppele.getOrDefault(partieUI.coequipier, 0) + 1
         if (partieUI.gagne) {
             nbWinJoueur[partieUI.nomJoueur] = nbWinJoueur.getOrDefault(partieUI.nomJoueur, 0) + 1
+            nbWinAppele[partieUI.coequipier] = nbWinAppele.getOrDefault(partieUI.coequipier, 0) + 1
         }
     }
-    WinrateLeaderboard(
-        modifier = modifier, nbWinJoueur = nbWinJoueur, nbPartiesJoueur = nbPartiesJoueur
-    )
-
+    Column(modifier = modifier) {
+        var tabSelected by remember { mutableStateOf(0) }
+        PrimaryTabRow(selectedTabIndex = tabSelected) {
+            Tab(selected = tabSelected == 0, onClick = { tabSelected = 0 }) {
+                Text(modifier = Modifier.padding(16.dp), text = "Preneur", fontSize = 20.sp)
+            }
+            Tab(selected = tabSelected == 1, onClick = { tabSelected = 1 }) {
+                Text(modifier = Modifier.padding(16.dp), text = "Appelé", fontSize = 20.sp)
+            }
+        }
+        if (tabSelected == 0) {
+            WinrateLeaderboard(nbWin = nbWinJoueur, nbParties = nbPartiesJoueur)
+        } else {
+            WinrateLeaderboard(nbWin = nbWinAppele, nbParties = nbPartieAppele)
+        }
+    }
 }
 
 @Composable
 fun WinrateLeaderboard(
-    modifier: Modifier = Modifier, nbWinJoueur: Map<String, Int>, nbPartiesJoueur: Map<String, Int>
+    modifier: Modifier = Modifier, nbWin: Map<String, Int>, nbParties: Map<String, Int>
 ) {
     val winrateJoueur: MutableMap<String, Float> = HashMap()
-    nbPartiesJoueur.forEach { (nomJoueur, nbParties) ->
-        winrateJoueur[nomJoueur] =
-            (nbWinJoueur.getOrDefault(nomJoueur, 0) / nbParties.toFloat()) * 100
+    nbParties.forEach { (nomJoueur, nbParties) ->
+        winrateJoueur[nomJoueur] = (nbWin.getOrDefault(nomJoueur, 0) / nbParties.toFloat()) * 100
     }
     Column(modifier = modifier) {
         val sortedWinrateJoueur = winrateJoueur.toSortedMap(compareByDescending<String> {
             winrateJoueur[it]
-        }.thenComparing(compareByDescending { nbPartiesJoueur[it] }).thenComparing(String::toString)
+        }.thenComparing(compareByDescending { nbParties[it] }).thenComparing(String::toString)
         )
         for (joueurWinrate in sortedWinrateJoueur) {
             WinrateRow(
                 modifier = Modifier.fillMaxWidth(),
                 nomJoueur = joueurWinrate.key,
                 winrate = joueurWinrate.value,
-                nbParties = nbPartiesJoueur.getOrDefault(joueurWinrate.key, 0),
-                nbWin = nbWinJoueur.getOrDefault(joueurWinrate.key, 0)
+                nbParties = nbParties.getOrDefault(joueurWinrate.key, 0),
+                nbWin = nbWin.getOrDefault(joueurWinrate.key, 0)
             )
             HorizontalDivider()
         }
@@ -127,9 +150,9 @@ fun LeaderboardScreenContentPreview() {
                 PartieUI(1L, "Corentin", CouleurEnum.TREFLE, true, "Tanguy"),
                 PartieUI(2L, "Corentin", CouleurEnum.CARREAU, false, "Tanguy"),
                 PartieUI(3L, "Tanguy", CouleurEnum.PIQUE, true, "Corentin"),
-                PartieUI(4L, "Tanguy", CouleurEnum.PIQUE, false, "Corentin"),
+                PartieUI(4L, "Tanguy", CouleurEnum.PIQUE, false, "Josh"),
                 PartieUI(5L, "Josh", CouleurEnum.PIQUE, false, "Corentin"),
-                PartieUI(6L, "Josh", CouleurEnum.PIQUE, true, "Corentin"),
+                PartieUI(6L, "Josh", CouleurEnum.PIQUE, true, "Tanguy"),
                 PartieUI(7L, "Josh", CouleurEnum.PIQUE, false, "Corentin"),
                 PartieUI(8L, "Josh", CouleurEnum.PIQUE, true, "Corentin"),
             )
--- app/src/main/java/com/berthias/tarotstats/screen/ListePartiesScreen.kt
+++ app/src/main/java/com/berthias/tarotstats/screen/ListePartiesScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -91,11 +92,13 @@ fun ListePartiesScreen(
             })
             FloatingActionButton(
                 modifier = Modifier
-                    .align(Alignment.BottomEnd)
+                    .align(Alignment.BottomCenter)
                     .padding(16.dp),
                 onClick = {
                     navigateToAddPartie()
-                }) {
+                },
+                shape = RoundedCornerShape(100)
+            ) {
                 Icon(Icons.Filled.Add, "add button")
             }
         }
