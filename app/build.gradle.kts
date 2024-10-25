56f308e19279f32bd9161f21159e77382bd4d92a
CBerthias <corentin.berthias@outlook.com>
2024-10-25 20:18:10 +0200

    version 1.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.berthias.tarotstats"
         minSdk = 29
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.3"
+        versionCode = 4
+        versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
